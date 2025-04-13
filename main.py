62de09f702881ac5856a94aa4dedd006662247f4
teampepev2 <141124321+teampepev2@users.noreply.github.com>
2025-04-13 15:59:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from handlers import start_command, menu_callback
 
-# Di bagian pendaftaran handler
+# Daftarkan handler ke dispatcher
 dispatcher.add_handler(start_command)
 dispatcher.add_handler(menu_callback)
